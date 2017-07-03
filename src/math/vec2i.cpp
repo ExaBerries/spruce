dbe58dee61230f6b5e46d49ae245b3fbdfb7d5db
ExaBerries <exaberriesdev@gmail.com>
2017-07-03 08:39:26 -0700

    vec2i

--- src/math/vec2i.cpp
+++ src/math/vec2i.cpp
@@ -1,4 +1,159 @@
 #include "vec2i.h"
 
 namespace spruce {
+	vec2i::vec2i() : x(0), y(0) {
+	}
+
+	vec2i::vec2i(const int& scalar) : x(scalar), y(scalar) {
+	}
+
+	vec2i::vec2i(const int& x, const int& y) : x(x), y(y){
+	}
+
+	float vec2i::mag2() const {
+		return x * x + y * y;
+	}
+
+	float vec2i::mag() const {
+		return sqrt(mag2());
+	}
+
+	float vec2i::dst2(const vec2i& vector) const {
+		float dx = vector.x - x;
+		float dy = vector.y - y;
+		return dx * dx + dy * dy;
+	}
+
+	float vec2i::dst(const vec2i& vector) const {
+		return sqrt(dst2(vector));
+	}
+
+	float vec2i::dot(const vec2i& vector) const {
+		return vector.x * x + vector.y * y;
+	}
+
+	vec2i& vec2i::add(const vec2i& vector) {
+		x += vector.x;
+		y += vector.y;
+		return *this;
+	}
+
+	vec2i& vec2i::sub(const vec2i& vector) {
+		x -= vector.x;
+		y -= vector.y;
+		return *this;
+	}
+
+	vec2i& vec2i::add(const int& value) {
+		x += value;
+		y += value;
+		return *this;
+	}
+
+	vec2i& vec2i::sub(const int& value) {
+		x -= value;
+		y -= value;
+		return *this;
+	}
+
+	vec2i& vec2i::scl(const int& value) {
+		x *= value;
+		y *= value;
+		return *this;
+	}
+
+	vec2i& vec2i::div(const int& value) {
+		x /= value;
+		y /= value;
+		return *this;
+	}
+
+	vec2i operator+(const vec2i left, const vec2i& right) {
+		int x = left.x + right.x;
+		int y = left.y + right.y;
+		return vec2i(x, y);
+	}
+
+	vec2i operator-(const vec2i left, const vec2i& right) {
+		int x = left.x - right.x;
+		int y = left.y - right.y;
+		return vec2i(x, y);
+	}
+
+	vec2i operator+(const vec2i left, int value) {
+		int x = left.x + value;
+		int y  = left.y + value;
+		return vec2i(x, y);
+	}
+
+	vec2i operator-(const vec2i left, int value) {
+		int x = left.x - value;
+		int y = left.y - value;
+		return vec2i(x, y);
+	}
+
+	vec2i operator*(const vec2i left, int value) {
+		int x = left.x * value;
+		int y = left.y * value;
+		return vec2i(x, y);
+	}
+
+	vec2i operator/(const vec2i left, int value) {
+		int x = left.x / value;
+		int y = left.y / value;
+		return vec2i(x, y);
+	}
+
+	bool operator==(const vec2i& left, const vec2i& right) {
+		return (left.x == right.x && left.y == right.y);
+	}
+
+	bool operator!=(const vec2i& left, const vec2i& right) {
+		return (left.x != right.x || left.y != right.y);
+	}
+
+	vec2i& vec2i::operator+=(const vec2i& vector) {
+		this->x += vector.x;
+		this->y += vector.y;
+		return *this;
+	}
+
+	vec2i& vec2i::operator-=(const vec2i& vector) {
+		this->x -= vector.x;
+		this->y -= vector.y;
+		return *this;
+	}
+
+	vec2i& vec2i::operator+=(const int& value) {
+		this->x += value;
+		this->y += value;
+		return *this;
+	}
+
+	vec2i& vec2i::operator-=(const int& value) {
+		this->x -= value;
+		this->y -= value;
+		return *this;
+	}
+
+	vec2i& vec2i::operator*=(const int& value) {
+		this->x *= value;
+		this->y *= value;
+		return *this;
+	}
+
+	vec2i& vec2i::operator/=(const int& value) {
+		this->x /= value;
+		this->y /= value;
+		return *this;
+	}
+
+	std::ostream& operator<<(std::ostream& stream, const vec2i& vector) {
+		stream << "vec2i(" << vector.x << ", " << vector.y << ")";
+		return stream;
+	}
+
+	extern void log(const vec2i& vector) {
+		std::cout << "vec2i(" << vector.x << ", " << vector.y << ")" << std::endl;
+	}
 }
--- src/math/vec2i.h
+++ src/math/vec2i.h
@@ -1,11 +1,51 @@
 #pragma once
 #include <iostream>
+#include <math.h>
 
 namespace spruce {
     struct vec2i {
         int x;
         int y;
+
+        vec2i();
+		vec2i(const int& scalar);
+		vec2i(const int& x, const int& y);
+
+		float mag2() const;
+		float mag() const;
+		float dst2(const vec2i& vector) const;
+		float dst(const vec2i& vector) const;
+		float dot(const vec2i& vector) const;
+
+		vec2i& add(const vec2i& vector);
+		vec2i& sub(const vec2i& vector);
+
+		vec2i& add(const int& value);
+		vec2i& sub(const int& value);
+		vec2i& scl(const int& value);
+		vec2i& div(const int& value);
+
+		friend vec2i operator+(const vec2i left, const vec2i& right);
+		friend vec2i operator-(const vec2i left, const vec2i& right);
+
+		friend vec2i operator+(const vec2i left, int value);
+		friend vec2i operator-(const vec2i left, int value);
+		friend vec2i operator*(const vec2i left, int value);
+		friend vec2i operator/(const vec2i left, int value);
+
+		bool operator==(const vec2i& vector) const;
+		bool operator!=(const vec2i& vector) const;
+
+		vec2i& operator+=(const vec2i& vector);
+		vec2i& operator-=(const vec2i& vector);
+
+		vec2i& operator+=(const int& value);
+		vec2i& operator-=(const int& value);
+		vec2i& operator*=(const int& value);
+		vec2i& operator/=(const int& value);
+
+		friend std::ostream& operator<<(std::ostream& stream, const vec2i& vector);
     };
 
-    void log(vec2i& vector);
+    extern void log(const vec2i& vector);
 }
