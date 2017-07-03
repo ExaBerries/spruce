f119597fda139c6c250f6da3fe25d15ddcdf4fb3
ExaBerries <exaberriesdev@gmail.com>
2017-07-03 09:14:05 -0700

    vec3f

--- src/math/vec3f.cpp
+++ src/math/vec3f.cpp
@@ -1,4 +1,202 @@
 #include "vec3f.h"
 
 namespace spruce {
+	vec3f::vec3f() : x(0), y(0), z(0) {
+	}
+
+	vec3f::vec3f(const float& scalar) : x(scalar), y(scalar), z(scalar) {
+	}
+
+	vec3f::vec3f(const vec2f& vector, const float& z) : x(vector.x), y(vector.y), z(z) {
+	}
+
+	vec3f::vec3f(const float& x, const float& y, const float& z) : x(x), y(y), z(z) {
+	}
+
+	float vec3f::mag2() const {
+		return x * x + y * y + z * z;
+	}
+
+	float vec3f::mag() const {
+		return sqrt(mag2());
+	}
+
+	vec3f& vec3f::nor() {
+		float mag2 = vec3f::mag2();
+		if (mag2 != 0 && mag2 != 1) {
+			float mag = sqrt(mag2);
+			x /= mag;
+			y /= mag;
+			z /= mag;
+		}
+		return *this;
+	}
+
+	float vec3f::dst2(const vec3f& vector) const {
+		float dx = vector.x - x;
+		float dy = vector.y - y;
+		float dz = vector.z - z;
+		return dx * dx + dy * dy + dz * dz;
+	}
+
+	float vec3f::dst(const vec3f& vector) const {
+		return sqrt(dst2(vector));
+	}
+
+	float vec3f::dot(const vec3f& vector) const {
+		return vector.x * x + vector.y * y + vector.z * z;
+	}
+
+	vec3f& vec3f::crs(const vec3f& vector) {
+		float x = this->y * vector.z - this->z * vector.y;
+		float y = this->z * vector.x - this->x * vector.z;
+		float z = this->x * vector.y - this->y * vector.x;
+		this->x = x;
+		this->y = y;
+		this->z = z;
+		return *this;
+	}
+
+	vec3f& vec3f::add(const vec3f& vector) {
+		x += vector.x;
+		y += vector.y;
+		z += vector.z;
+		return *this;
+	}
+
+	vec3f& vec3f::sub(const vec3f& vector) {
+		x -= vector.x;
+		y -= vector.y;
+		z -= vector.z;
+		return *this;
+	}
+
+	vec3f& vec3f::add(const float& value) {
+		x += value;
+		y += value;
+		z += value;
+		return *this;
+	}
+
+	vec3f& vec3f::sub(const float& value) {
+		x -= value;
+		y -= value;
+		z -= value;
+		return *this;
+	}
+
+	vec3f& vec3f::scl(const float& value) {
+		x *= value;
+		y *= value;
+		z *= value;
+		return *this;
+	}
+
+	vec3f& vec3f::div(const float& value) {
+		x /= value;
+		y /= value;
+		z /= value;
+		return *this;
+	}
+
+	vec3f operator+(const vec3f left, const vec3f& right) {
+		float x = left.x + right.x;
+		float y = left.y + right.y;
+		float z = left.z + right.z;
+		return vec3f(x, y, z);
+	}
+
+	vec3f operator-(const vec3f left, const vec3f& right) {
+		float x = left.x - right.x;
+		float y = left.y - right.y;
+		float z = left.z - right.z;
+		return vec3f(x, y, z);
+	}
+
+	vec3f operator+(const vec3f left, float value) {
+		float x = left.x + value;
+		float y = left.y + value;
+		float z = left.z + value;
+		return vec3f(x, y, z);
+	}
+
+	vec3f operator-(const vec3f left, float value) {
+		float x = left.x - value;
+		float y = left.y - value;
+		float z = left.z - value;
+		return vec3f(x, y, z);
+	}
+
+	vec3f operator*(const vec3f left, float value) {
+		float x = left.x * value;
+		float y = left.y * value;
+		float z = left.z * value;
+		return vec3f(x, y, z);
+	}
+
+	vec3f operator/(const vec3f left, float value) {
+		float x = left.x / value;
+		float y = left.y / value;
+		float z = left.z / value;
+		return vec3f(x, y, z);
+	}
+
+	bool operator==(const vec3f& left, const vec3f& right) {
+		return (left.x == right.x && left.y == right.y && left.z == right.z);
+	}
+
+	bool operator!=(const vec3f& left, const vec3f& right) {
+		return (left.x != right.x || left.y != right.y || left.z != right.z);
+	}
+
+	vec3f& vec3f::operator+=(const vec3f& vector) {
+		this->x += vector.x;
+		this->y += vector.y;
+		this->z += vector.z;
+		return *this;
+	}
+
+	vec3f& vec3f::operator-=(const vec3f& vector) {
+		this->x -= vector.x;
+		this->y -= vector.y;
+		this->z -= vector.z;
+		return *this;
+	}
+
+	vec3f& vec3f::operator+=(const float& value) {
+		this->x += value;
+		this->y += value;
+		this->z += value;
+		return *this;
+	}
+
+	vec3f& vec3f::operator-=(const float& value) {
+		this->x -= value;
+		this->y -= value;
+		this->z -= value;
+		return *this;
+	}
+
+	vec3f& vec3f::operator*=(const float& value) {
+		this->x *= value;
+		this->y *= value;
+		this->z *= value;
+		return *this;
+	}
+
+	vec3f& vec3f::operator/=(const float& value) {
+		this->x /= value;
+		this->y /= value;
+		this->z /= value;
+		return *this;
+	}
+
+	std::ostream& operator<<(std::ostream& stream, const vec2f& vector) {
+		std::cout << "vec3f(" << vector.x << ", " << vector.y << ")" << std::endl;
+		return stream;
+	}
+
+	extern void log(const vec3f& vector) {
+		std::cout << "vec3f(" << vector.x << ", " << vector.y << ")" << std::endl;
+	}
 }
--- src/math/vec3f.h
+++ src/math/vec3f.h
@@ -1,12 +1,56 @@
 #pragma once
 #include <iostream>
+#include "vec2f.h"
+#include <math.h>
 
 namespace spruce {
-    struct vec3f {
-        float x;
-        float y;
-        float z;
-    };
+	struct vec3f {
+		float x;
+		float y;
+		float z;
 
-    void log(vec3f& vector);
+		vec3f();
+		vec3f(const float& scalar);
+		vec3f(const vec2f& vector, const float& z);
+		vec3f(const float& x, const float& y, const float& z);
+
+		float mag2() const;
+		float mag() const;
+		vec3f& nor();
+		float dst2(const vec3f& vector) const;
+		float dst(const vec3f& vector) const;
+		float dot(const vec3f& vector) const;
+		vec3f& crs(const vec3f& vector);
+
+		vec3f& add(const vec3f& vector);
+		vec3f& sub(const vec3f& vector);
+
+		vec3f& add(const float& value);
+		vec3f& sub(const float& value);
+		vec3f& scl(const float& value);
+		vec3f& div(const float& value);
+
+		friend vec3f operator+(const vec3f left, const vec3f& right);
+		friend vec3f operator-(const vec3f left, const vec3f& right);
+
+		friend vec3f operator+(const vec3f left, float value);
+		friend vec3f operator-(const vec3f left, float value);
+		friend vec3f operator*(const vec3f left, float value);
+		friend vec3f operator/(const vec3f left, float value);
+
+		bool operator==(const vec3f& vector) const;
+		bool operator!=(const vec3f& vector) const;
+
+		vec3f& operator+=(const vec3f& vector);
+		vec3f& operator-=(const vec3f& vector);
+
+		vec3f& operator+=(const float& value);
+		vec3f& operator-=(const float& value);
+		vec3f& operator*=(const float& value);
+		vec3f& operator/=(const float& value);
+
+		friend std::ostream& operator<<(std::ostream& stream, const vec2f& vector);
+	};
+
+	extern void log(const vec3f& vector);
 }
