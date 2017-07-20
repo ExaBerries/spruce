aad3377f61c6031d12fb1c01fedb54daf4c1c976
ExaBerries <exaberriesdev@gmail.com>
2017-07-20 14:31:32 -0700

    add math classes to spruce.h

--- src/spruce.h
+++ src/spruce.h
@@ -2,4 +2,9 @@
 
 #include <common.h>
 #include <Application.h>
+#include <math/vec2f.h>
+#include <math/vec2i.h>
+#include <math/vec3f.h>
+#include <math/mat4f.h>
+#include <math/quaternion.h>
 #include <graphics/graphics.h>
