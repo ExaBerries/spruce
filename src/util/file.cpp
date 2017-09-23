07074bf383f599ed3b1ce05cdcb1534930f16f2d
ExaBerries <exaberriesdev@gmail.com>
2017-09-23 09:14:17 -0700

    read and write binary files

--- src/util/file.cpp
+++ src/util/file.cpp
@@ -2,7 +2,7 @@
 
 namespace spruce {
 	namespace util {
-		string readFile(const char* filepath) {
+		string readFileTxt(const char* filepath) {
 			FILE* file = fopen(filepath, "rt");
 			if (file != NULL) {
 				fseek(file, 0, SEEK_END);
@@ -16,24 +16,54 @@ namespace spruce {
 				delete[] data;
 				return result;
 			}
-			return ":(";
+			return "";
 		}
 
-		string readFile(const string& filepath) {
-			FILE* file = fopen(filepath.c_str(), "rt");
+		string readFileTxt(const string& filepath) {
+			return readFileTxt(filepath.c_str());
+		}
+
+		uint8* readFileBin(const char* filepath) {
+			FILE* file = fopen(filepath, "rb");
 			if (file != NULL) {
 				fseek(file, 0, SEEK_END);
 				unsigned long length = ftell(file);
-				char* data = new char[length + 1];
+				uint8* data = new uint8[length + 1];
 				memset(data, 0, length + 1);
 				fseek(file, 0, SEEK_SET);
-				fread(data, sizeof(char), length, file);
+				fread(data, sizeof(uint8), length, file);
 				fclose(file);
-				string result(data);
-				delete[] data;
-				return result;
+				return data;
 			}
-			return ":(";
+			return nullptr;
+		}
+
+		uint8* readFileBin(const string& filepath) {
+			return readFileBin(filepath.c_str());
+		}
+
+		void writeFileTxt(const char* filepath, const char* string) {
+			FILE* file = fopen(filepath, "wt");
+			if (file != NULL) {
+				fputs(string, file);
+				fclose(file);
+			}
+		}
+
+		void writeFileTxt(const string& filepath, string& string){
+			writeFileTxt(filepath.c_str(), string.c_str());
+		}
+
+		void writeFileBin(const char* filepath, uint8* data, uint32& count) {
+			FILE* file = fopen(filepath, "wb");
+			if (file != NULL) {
+				fwrite(data, sizeof(uint8), count, file);
+				fclose(file);
+			}
+		}
+
+		void writeFileBin(const string& filepath, uint8* data, uint32& count) {
+			writeFileBin(filepath.c_str(), data, count);
 		}
 	}
 }
--- src/util/file.h
+++ src/util/file.h
@@ -3,7 +3,14 @@
 
 namespace spruce {
 	namespace util {
-		string readFile(const char* filepath);
-		string readFile(const string& filepath);
+		string readFileTxt(const char* filepath);
+		string readFileTxt(const string& filepath);
+		uint8* readFileBin(const char* filepath);
+		uint8* readFileBin(const string& filepath);
+
+		void writeFileTxt(const char* filepath, const char* string);
+		void writeFileTxt(const string& filepath, string& string);
+		void writeFileBin(const char* filepath, uint8* data, uint32& count);
+		void writeFileBin(const string& filepath, uint8* data, uint32& count);
 	}
 }
