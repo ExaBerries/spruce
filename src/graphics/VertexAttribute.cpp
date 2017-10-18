285b9f16565e7d2c4dfd269f2432978f0c4cda7c
ExaBerries <exaberriesdev@gmail.com>
2017-10-18 14:01:57 -0700

    change VertexAttribute default values

--- src/graphics/VertexAttribute.cpp
+++ src/graphics/VertexAttribute.cpp
@@ -1,7 +1,7 @@
 #include <graphics/VertexAttribute.h>
 
 namespace spruce {
-	VertexAttribute::VertexAttribute() : name("position"), size(3) {
+	VertexAttribute::VertexAttribute() : name("null"), size(0) {
 	}
 
 	VertexAttribute::VertexAttribute(string name, uint8 size) : name(name), size(size) {
