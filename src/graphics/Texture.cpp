6c706c337aa04d53affcd5692d4a50838789d7bf
ExaBerries <exaberriesdev@gmail.com>
2018-09-16 09:17:44 -0700

    move Texture to graphics namespace

--- src/graphics/Texture.cpp
+++ src/graphics/Texture.cpp
@@ -4,21 +4,23 @@
 #include <graphics/graphics.h>
 
 namespace spruce {
-	Texture::Texture(PixelFormat format, buffer<uint8> data, uint16& width, uint16& height) : data(data) {
-		this->format = format;
-		this->width = width;
-		this->height = height;
-	}
+	namespace graphics {
+		Texture::Texture(PixelFormat format, buffer<uint8> data, uint16& width, uint16& height) : data(data) {
+			this->format = format;
+			this->width = width;
+			this->height = height;
+		}
 
-	Texture::~Texture() {
-		data.free();
-	}
+		Texture::~Texture() {
+			data.free();
+		}
 
-	void Texture::bind() {
-		graphics::getCommandBuffer().add(new TextureBindCommand(this));
-	}
+		void Texture::bind() {
+			graphics::getCommandBuffer().add(new TextureBindCommand(this));
+		}
 
-	void Texture::unbind() {
-		graphics::getCommandBuffer().add(new TextureUnbindCommand(this));
+		void Texture::unbind() {
+			graphics::getCommandBuffer().add(new TextureUnbindCommand(this));
+		}
 	}
 }
