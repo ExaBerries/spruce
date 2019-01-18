d182a49a12093961deca19ce8ddfd691267bcf98
exaberries <exaberriesdev@gmail.com>
2019-01-18 11:49:03 -0800

    use padded vec3f instead of float array

--- core/src/math/vec/vec3fImpl.h
+++ core/src/math/vec/vec3fImpl.h
@@ -1,22 +1,21 @@
 #pragma once
 #include <xmmintrin.h>
 
-
 namespace spruce {
 	inline vec3f operator+(const vec3f& left, const vec3f& right) {
 		__m128 l = _mm_load_ps(&left.x);
 		__m128 r = _mm_load_ps(&right.x);
-		float out[4];
-		_mm_store_ps(out, _mm_add_ps(l, r));
-		return *((vec3f*) &out);
+		vec3f out;
+		_mm_store_ps(&out.x, _mm_add_ps(l, r));
+		return out;
 	}
 
 	inline vec3f operator*(const vec3f& left, float value) {
 		__m128 l = _mm_load_ps(&left.x);
 		__m128 r = _mm_load_ps1(&value);
-		float out[4];
-		_mm_store_ps(out, _mm_mul_ps(l, r));
-		return *((vec3f*) &out);
+		vec3f out;
+		_mm_store_ps(&out.x, _mm_mul_ps(l, r));
+		return out;
 	}
 }
 
