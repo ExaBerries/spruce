253ce69dfc0e4752a81460a918decfded13ac900
exaberries <exaberriesdev@gmail.com>
2019-01-22 13:34:45 -0800

    add SPRUCE_MATH_ALIAS macro to alias vec quat and mat into namespace

--- core/include/spruce.h
+++ core/include/spruce.h
@@ -12,6 +12,17 @@
 #include <memory/memory.h>
 #include <SpruceEngine.h>
 
+#define SPRUCE_MATH_ALIAS \
+	using spruce::vec2f;\
+	using spruce::vec2d;\
+	using spruce::vec2i;\
+	using spruce::vec3f;\
+	using spruce::vec3d;\
+	using spruce::vec3i;\
+	using spruce::vec4f;\
+	using spruce::quaternion;\
+	using spruce::mat4f;
+
 #ifdef TREE_ALIAS
 #define 🌲 spruce;
 #endif
