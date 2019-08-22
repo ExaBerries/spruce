44be4f6f4b2d266a7bfc8848dff5ee9e8c126f98
exaberries <exaberriesdev@gmail.com>
2019-08-21 23:24:10 -0700

    vec2f inline operators

---
+++ core/bench/src/vec2fbench.cpp
@@ -0,0 +1,123 @@
+#include <benchmark/benchmark.h>
+#include <math/vec/vec2f.h>
+
+using namespace spruce;
+
+static void vec2fConstruct(benchmark::State& state) {
+	for (auto _ : state) {
+		vec2f v;
+		benchmark::DoNotOptimize(v);
+	}
+}
+BENCHMARK(vec2fConstruct);
+
+static void vec2fScale(benchmark::State& state) {
+	vec2f a(4, 4);
+	float scale = 2;
+	for (auto _ : state) {
+		vec2f b = a * scale;
+		benchmark::DoNotOptimize(b);
+	}
+}
+BENCHMARK(vec2fScale);
+
+static void vec2fAddVec(benchmark::State& state) {
+	vec2f a(1, 0);
+	vec2f b(0, 1);
+	for (auto _ : state) {
+		vec2f c = a + b;
+		benchmark::DoNotOptimize(c);
+	}
+}
+BENCHMARK(vec2fAddVec);
+
+static void vec2fScaleMulti(benchmark::State& state) {
+	constexpr uint64 NUM = 100000;
+	struct TestData {
+		vec2f vector;
+		float scale;
+	};
+	TestData data[NUM];
+	for (uint64 i = 0; i < NUM; i++) {
+		data[i].vector.set(i, i);
+		data[i].scale = i;
+	}
+	for (auto _ : state) {
+		for (uint64 i = 0; i < NUM; i++) {
+			vec2f vec = data[i].vector * data[i].scale;
+			benchmark::DoNotOptimize(vec);
+		}
+	}
+	state.SetBytesProcessed(state.iterations() * sizeof(data));
+	state.SetItemsProcessed(state.iterations() * NUM);
+}
+BENCHMARK(vec2fScaleMulti);
+
+static void vec2fAddMulti(benchmark::State& state) {
+	constexpr uint64 NUM = 100000;
+	struct TestData {
+		vec2f a;
+		vec2f b;
+	};
+	TestData data[NUM];
+	for (uint64 i = 0; i < NUM; i++) {
+		data[i].a.set(i, i);
+		data[i].b = i;
+	}
+	for (auto _ : state) {
+		for (uint64 i = 0; i < NUM; i++) {
+			vec2f vec = data[i].a + data[i].b;
+			benchmark::DoNotOptimize(vec);
+		}
+	}
+	state.SetBytesProcessed(state.iterations() * sizeof(data));
+	state.SetItemsProcessed(state.iterations() * NUM);
+}
+BENCHMARK(vec2fAddMulti);
+
+static void vec2fSubMulti(benchmark::State& state) {
+	constexpr uint64 NUM = 100000;
+	struct TestData {
+		vec2f a;
+		vec2f b;
+	};
+	TestData data[NUM];
+	for (uint64 i = 0; i < NUM; i++) {
+		data[i].a.set(i, i);
+		data[i].b = i;
+	}
+	for (auto _ : state) {
+		for (uint64 i = 0; i < NUM; i++) {
+			vec2f vec = data[i].a - data[i].b;
+			benchmark::DoNotOptimize(vec);
+		}
+	}
+	state.SetBytesProcessed(state.iterations() * sizeof(data));
+	state.SetItemsProcessed(state.iterations() * NUM);
+}
+BENCHMARK(vec2fSubMulti);
+
+#ifdef __APPLE__
+#include <simd/simd.h>
+
+static void vec2fAddMultiSIMD(benchmark::State& state) {
+	constexpr uint64 NUM = 100000;
+	struct TestData {
+		vector_float2 a;
+		vector_float2 b;
+	};
+	TestData data[NUM];
+	for (uint64 i = 0; i < NUM; i++) {
+		data[i].a = {(float)i, (float)i};
+		data[i].b = {(float)i, (float)i};
+	}
+	for (auto _ : state) {
+		for (uint64 i = 0; i < NUM; i++) {
+			volatile vector_float2 vec = data[i].a + data[i].b;
+		}
+	}
+	state.SetBytesProcessed(state.iterations() * sizeof(data));
+	state.SetItemsProcessed(state.iterations() * NUM);
+}
+BENCHMARK(vec2fAddMultiSIMD);
+#endif
