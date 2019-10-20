5bd77bc25218f3010f688c5e7628be8a15e62002
exaberries <exaberriesdev@gmail.com>
2019-10-20 00:12:58 -0700

    update clang-tidy checks

--- gen-.clang-tidy.sh
+++ gen-.clang-tidy.sh
@@ -1,5 +1,8 @@
 checks='*,
 
+-android-*
+-openmp-*
+
 -clang-analyzer-optin.osx.cocoa.localizability.NonLocalizedStringChecker,
 -clang-analyzer-nullability.NullabilityBase,
 -clang-analyzer-osx.cocoa.RetainCountBase,
@@ -15,23 +18,27 @@ checks='*,
 -hicpp-member-init,
 -hicpp-explicit-conversions,
 -hicpp-use-auto,
+-hicpp-no-array-decay,
 
 -google-readability-namespace-comments,
 -google-runtime-references,
 -google-explicit-constructor,
 -google-objc-global-variable-declaration,
+-google-objc-function-naming,
+-google-build-using-namespace,
 
 -cppcoreguidelines-avoid-c-arrays,
 -cppcoreguidelines-non-private-member-variables-in-classes,
--cppcoreguidelines-pro-type-reinterpret-cast,
 -cppcoreguidelines-avoid-magic-numbers,
 -cppcoreguidelines-no-malloc,
--cppcoreguidelines-pro-bounds-pointer-arithmetic,
--cppcoreguidelines-pro-bounds-constant-array-index,
 -cppcoreguidelines-owning-memory,
 -cppcoreguidelines-macro-usage,
+-cppcoreguidelines-pro-type-reinterpret-cast,
+-cppcoreguidelines-pro-bounds-pointer-arithmetic,
+-cppcoreguidelines-pro-bounds-constant-array-index,
 -cppcoreguidelines-pro-type-member-init,
 -cppcoreguidelines-pro-type-static-cast-downcast,
+-cppcoreguidelines-pro-bounds-array-to-pointer-decay,
 
 -modernize-concat-nested-namespaces,
 -modernize-use-trailing-return-type,
@@ -49,6 +56,7 @@ checks='*,
 -readability-magic-numbers,
 -readability-uppercase-literal-suffix,
 -readability-else-after-return,
+-readability-implicit-bool-conversion,
 
 -misc-non-private-member-variables-in-classes,
 
